5987a1147c84c65d72eb83c03601446032c5acee
enimiste <nouni.elbachir@gmail.com>
2022-03-25 14:09:19 +0100

    add readme file

--- corpus_main_script.py
+++ corpus_main_script.py
@@ -1,12 +1,3 @@
-"""
-Python 3.x
-pip install mysql-connector-python
-pip install concurrent-log-handler
-
-pip install cchardet wia :
-    - Download this file first https://files.pythonhosted.org/packages/19/c7/fa589626997dd07bd87d9269342ccb74b1720384a4d739a1872bd84fbe68/chardet-4.0.0-py2.py3-none-any.whl
-    - pip install .\chardet-4.0.0-py2.py3-none-any.whl
-"""
 from multiprocessing import Queue, Process
 import os
 import signal
